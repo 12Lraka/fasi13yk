0563e7b6bbeb6697a184532fa3a416cfafcb222e
12Lraka <98644795+12Lraka@users.noreply.github.com>
2026-08-25 18:49:15 +0700

    Update Navbar.tsx

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -74,7 +74,7 @@ export const Navbar: React.FC<NavbarProps> = ({
                 </span>
               </div>
               <p className="text-xs text-emerald-300 font-normal">
-                Sistem Pendaftaran, Validasi Usia & Live Score
+                Pendaftaran, Validasi Usia & Live Score
               </p>
             </div>
           </div>
